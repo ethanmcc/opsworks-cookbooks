22fda6c35096976f9666d0a3f9dd3fd21c0f1c64
Ethan McCreadie <ethanmcc@gmail.com>
2014-09-16 14:04:15 -0400

    admin: Support admin role

--- sftp/recipes/default.rb
+++ sftp/recipes/default.rb
@@ -6,21 +6,47 @@ group "sftp" do
     action :create
 end
 
+user node[:sftp][:admin_username] do
+    home "/home/#{node[:sftp][:admin_username]}"
+    action :create
+end
+
+directory "/home/#{node[:sftp][:admin_username]}/.ssh" do
+    owner node[:sftp][:admin_username]
+    mode 00700
+    action :create
+end
+
+file "/home/#{node[:sftp][:admin_username]}/.ssh/authorized_keys" do
+    owner node[:sftp][:admin_username]
+    content node[:sftp][:admin_public_key]
+    mode 00600
+    action :create
+end
+
+directory "/var/sftproot" do
+    owner "root"
+    group "root"
+    mode 00755
+    action :create
+end
+
 node[:sftp][:users].each do |usr|
     user usr.username do
-        home "/home/#{usr.username}"
+        home "/var/sftproot/#{usr.username}"
         password usr.password
         shell "/sbin/nologin"
         action :create
     end
 
-    directory "/home/#{usr.username}" do
+    directory "/var/sftproot/#{usr.username}" do
         owner "root"
         group "root"
-        mode "0755"
+        mode 00755
+        action :create
     end
 
-    directory "/home/#{usr.username}/upload" do
+    directory "/var/sftproot/#{usr.username}/upload" do
         owner usr.username
     end
 
@@ -31,15 +57,15 @@ node[:sftp][:users].each do |usr|
 end
 
 execute "restart_sshd" do
-  command "/etc/init.d/sshd restart"
-  action :nothing
+    command "/etc/init.d/sshd restart"
+    action :nothing
 end
 
 template "/etc/ssh/sshd_config" do
     source "sshd_config.erb"
     owner "root"
     group "root"
-    mode "0600"
+    mode 00600
     action :create
     notifies :run, 'execute[restart_sshd]', :delayed
 end
